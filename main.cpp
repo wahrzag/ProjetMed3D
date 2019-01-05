fbc9450be30676e4ddef939f270c73c9888f932d
kroncoli <kroncoli@prodpeda-x2go-bionic3.fdsetu.infra.umontpellier.fr>
2019-01-05 04:42:02 +0100

    x2go rame tellement du cul

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 using namespace cimg_library;
 using namespace std;
 
+#define NALDYDONE 0
+#define IGNORE -1
+
+float distanceInt(int p1,int p2){
+  return (p1<p2)?p2-p1:p1-p2;
+}
+
 int clamp(int val, int bornInf, int bornSup)
 {
 	return (val<bornInf)?bornInf:(val>bornSup)?bornSup:val;
@@ -25,6 +32,20 @@ void Seuil(CImg<> imgInit, CImg<>* imgTrait, int seuil, int* dim)
       		}
     	}
   	}
+}
+
+void remiseAZero(CImg<>* imgTrait, int* dim)
+{
+  for(int i = 0; i < dim[0]; i++)
+  {
+    for(int j = 0; j < dim[1]; j++)
+    {
+      for(int k = 0; k < dim[2]; k++)
+      {
+        (*imgTrait)(i,j,k) = 0;
+      }
+    }
+  }
 }
 
 void CopyImg(CImg<> imgInit, CImg<>* imgTrait, int* dim)
@@ -99,18 +120,95 @@ void regionGrowing(CImg<> imgInit, CImg<>* imgTrait, vector<int> seedPoint, bool
 	(*fini) = true;
 }
 
-void remiseAZero(CImg<>* imgTrait, int* dim)
-{
-	for(int i = 0; i < dim[0]; i++)
-	{
-		for(int j = 0; j < dim[1]; j++)
-		{
-			for(int k = 0; k < dim[2]; k++)
-			{
-				(*imgTrait)(i,j,k) = 0;
-			}
-		}
-	}
+void regionGrowingRec(CImg<> imgInit,CImg<>* labels,int* dim,int* seedPoint,int* threshold,int label){
+  int i = seedPoint[0];
+  int j = seedPoint[1];
+  int k = seedPoint[2];
+  (*labels)(i,j,k) = label;
+  for(i=-1;i<=1;i++)
+  {
+    for(j=-1;j<=1;j++)
+    {
+      for(k=-1;k<=1;k++)
+      { 
+        if((*labels)(seedPoint[0]+i,seedPoint[1]+j,seedPoint[2]+k) == NALDYDONE && distanceInt(imgInit(seedPoint[0],seedPoint[1],seedPoint[2]),imgInit(seedPoint[0]+i,seedPoint[1]+j,seedPoint[2]+k))<*threshold){
+          int newSeed[3] = {seedPoint[0]+i,seedPoint[1]+j,seedPoint[2]+k};
+          regionGrowingRec(imgInit,labels,dim,newSeed,threshold,label);
+        }
+      }
+    }
+  }
+}
+
+void regionGrowingPreT(CImg<> imgInit,CImg<>* labels,int* dim){
+  for(int i = 0; i < dim[0]; i++)
+  {
+    for(int j = 0; j < dim[1]; j++)
+    {
+      for(int k = 0; k < dim[2]; k++)
+      {
+        if ((imgInit)(i,j,k) == 0) (*labels)(i,j,k)=IGNORE;
+        else{
+          (*labels)(i,j,k)=NALDYDONE;
+        } 
+      }
+    }
+  }
+}
+
+void PaintWithLabels(CImg<> imgInit,CImg<>* imgTrait,CImg<> labels,int* dim){
+  int count=0;
+  for(int i = 0; i < dim[0]; i++)
+  {
+    for(int j = 0; j < dim[1]; j++)
+    {
+      for(int k = 0; k < dim[2]; k++)
+      {
+        if (labels(i,j,k) >= 1){
+          (*imgTrait)(i,j,k)=imgInit(i,j,k);
+          count++;
+        }
+        else (*imgTrait)(i,j,k)=0;
+      }
+    }
+  }
+  fprintf(stderr,"nb voxels : %d\n",count);
+}
+
+void firstContact(CImg<> imgInit, CImg<>* labels, CImgDisplay* display,int* dim,int* threshold){
+  regionGrowingPreT(imgInit,labels,dim);
+  fprintf(stderr,"Debut du region growing\n");
+  int numeroImage=dim[2]/2;
+  int clicX, clicY, clicZ;
+  float Cx = (float) dim[0] / (float) (*display).width();
+  float Cy = (float) dim[1] / (float) (*display).height();
+  while(!(*display).is_closed())
+  {
+      if ((*display).is_keyESC())
+      {
+          break;
+      }
+    
+      if ((*display).wheel())
+      {
+          numeroImage += (*display).wheel();
+          numeroImage = clamp(numeroImage,0,dim[2]);
+          (*display).set_wheel();
+      }
+      if ((*display).button()&1)
+      {
+        clicX = (*display).mouse_x() * Cx;
+        clicY = (*display).mouse_y() * Cy;
+        clicZ = numeroImage;
+        int seed[3] = {clicX, clicY, clicZ};
+        regionGrowingRec(imgInit, labels, dim,seed,threshold,1);
+        return;
+      }
+      CImg<> imgAffiche = imgInit.get_slice(numeroImage);
+      (*display).display(imgAffiche);
+      (*display).wait();
+    }
+    fprintf(stderr,"Fin du region growing\n");
 }
 
 int main(int argc, char *argv[])
@@ -119,37 +217,35 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-  		printf("Usage: fichier\n"); 
-  		exit (1) ;
-  	}
+ 		printf("Usage: fichier\n"); 
+ 		exit (1) ;
+ 	}
 
-  	sscanf (argv[1],"%s",cfichierlu);
-  	CImg<> imgInit, imgTrait, imgAffiche;
-  	float voxelsize[3];
+ 	sscanf (argv[1],"%s",cfichierlu);
+ 	CImg<> imgInit, imgTrait, labels;
+ 	float voxelsize[3];
+  imgInit.load_analyze(cfichierlu, voxelsize);
+  imgTrait.load_analyze(cfichierlu, voxelsize);
+  labels.load_analyze(cfichierlu,voxelsize);
 
-  	imgInit.load_analyze(cfichierlu, voxelsize);
-  	imgTrait.load_analyze(cfichierlu, voxelsize);
-
-  	int dim[] = {imgInit.width(),imgInit.height(),imgInit.depth()};
+  int dim[] = {imgInit.width(),imgInit.height(),imgInit.depth()};
  	
  	CImgDisplay display(imgInit);
 
-  	int seuil = 30;
+  int seuil = 30;
  	int numeroImage=dim[2]/2;
 
-  	pretraitement(imgInit,&imgTrait,&display,&seuil,dim);
+  pretraitement(imgInit,&imgTrait,&display,&seuil,dim);
+  fprintf(stderr, "Le filtre passe-haut est choisi à %d\n",seuil);
 
-  	int clicX, clicY, clicZ;
-  	float Cx = (float) dim[0] / (float) display.width();
-  	float Cy = (float) dim[1] / (float) display.height();
+  const unsigned char gray[] = {255};
 
-  	const unsigned char gray[] = {255};
+  CopyImg(imgTrait,&imgInit,dim);
+  int threshold = 5;
+  firstContact(imgInit,&labels,&display,dim,&threshold);
+  PaintWithLabels(imgInit,&imgTrait,labels,dim);
 
-  	CopyImg(imgTrait,&imgInit,dim);
-
-  	remiseAZero(&imgTrait, dim);
-
-  	bool testFini = false;
+  /*bool testFini = false;
 
  	while(!display.is_closed())
  	{
@@ -186,7 +282,7 @@ int main(int argc, char *argv[])
     	
     	display.display(imgAffiche);
   		display.wait();
-    }
+    }*/
 
     //imgTrait.save_analyze("result.hdr", voxelsize);
 
